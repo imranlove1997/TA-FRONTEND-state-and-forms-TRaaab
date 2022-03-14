c0481cc193124d3a130f64f863a42a8c5a4de624
Imran Khan <imranlove1997@gmail.com>
2022-03-14 12:04:12 +0530

    React

---
+++ block-BRaabw/ecommerce/src/App.js
@@ -0,0 +1,182 @@
+import {TiShoppingCart} from "react-icons/ti";
+
+function App() {
+    return (
+        <>
+        <div className="sidebar">
+            <p>Sizes:</p>
+            <ul>
+                <li><a href="#">XS</a></li>
+                <li><a href="#">XS</a></li>
+                <li><a href="#">XS</a></li>
+                <li><a href="#">XS</a></li>
+                <li><a href="#">XS</a></li>
+            </ul>
+        </div>
+        <div className="body-text"> 
+        <div className="cart">
+        <button><TiShoppingCart /></button>
+        </div>
+        <main>
+            <p>Total Products</p>
+        </main>
+        <div className="order-product">
+            <div className="product">
+                <div className="product-image">
+                <p>Free Shipping</p>
+                <div className="product-image-front">
+                <img src="/static/products/100_1.jpg" />
+                </div>
+                <div className="product-image-back">
+                    <img src="/static/products/101_1.jpg" />
+                    </div>
+                </div>
+                <div className="product-title">
+                    <h2>Product One</h2>
+                    <div>
+                    <p>Dollars</p>
+                    <p>size</p>
+                    </div>
+                </div>
+                <button>Click here</button>
+            </div>
+            <div className="product">
+                <div className="product-image">
+                <p>Free Shipping</p>
+                <div className="product-image-front">
+                <img src="/static/products/100_1.jpg" />
+                </div>
+                <div className="product-image-back">
+                    <img src="/static/products/101_1.jpg" />
+                    </div>
+                </div>
+                <div className="product-title">
+                    <h2>Product One</h2>
+                    <div>
+                    <p>Dollars</p>
+                    <p>size</p>
+                    </div>
+                </div>
+                <button>Click here</button>
+            </div>
+            <div className="product">
+                <div className="product-image">
+                <p>Free Shipping</p>
+                <div className="product-image-front">
+                <img src="/static/products/100_1.jpg" />
+                </div>
+                <div className="product-image-back">
+                    <img src="/static/products/101_1.jpg" />
+                    </div>
+                </div>
+                <div className="product-title">
+                    <h2>Product One</h2>
+                    <div>
+                    <p>Dollars</p>
+                    <p>size</p>
+                    </div>
+                </div>
+                <button>Click here</button>
+            </div>
+            <div className="product">
+                <div className="product-image">
+                <p>Free Shipping</p>
+                <div className="product-image-front">
+                <img src="/static/products/100_1.jpg" />
+                </div>
+                <div className="product-image-back">
+                    <img src="/static/products/101_1.jpg" />
+                    </div>
+                </div>
+                <div className="product-title">
+                    <h2>Product One</h2>
+                    <div>
+                    <p>Dollars</p>
+                    <p>size</p>
+                    </div>
+                </div>
+                <button>Click here</button>
+            </div>
+            <div className="product">
+                <div className="product-image">
+                <p>Free Shipping</p>
+                <div className="product-image-front">
+                <img src="/static/products/100_1.jpg" />
+                </div>
+                <div className="product-image-back">
+                    <img src="/static/products/101_1.jpg" />
+                    </div>
+                </div>
+                <div className="product-title">
+                    <h2>Product One</h2>
+                    <div>
+                    <p>Dollars</p>
+                    <p>size</p>
+                    </div>
+                </div>
+                <button>Click here</button>
+            </div>
+            <div className="product">
+                <div className="product-image">
+                <p>Free Shipping</p>
+                <div className="product-image-front">
+                <img src="/static/products/100_1.jpg" />
+                </div>
+                <div className="product-image-back">
+                    <img src="/static/products/101_1.jpg" />
+                    </div>
+                </div>
+                <div className="product-title">
+                    <h2>Product One</h2>
+                    <div>
+                    <p>Dollars</p>
+                    <p>size</p>
+                    </div>
+                </div>
+                <button>Click here</button>
+            </div>
+            <div className="product">
+                <div className="product-image">
+                <p>Free Shipping</p>
+                <div className="product-image-front">
+                <img src="/static/products/100_1.jpg" />
+                </div>
+                <div className="product-image-back">
+                    <img src="/static/products/101_1.jpg" />
+                    </div>
+                </div>
+                <div className="product-title">
+                    <h2>Product One</h2>
+                    <div>
+                    <p>Dollars</p>
+                    <p>size</p>
+                    </div>
+                </div>
+                <button>Click here</button>
+            </div>
+            <div className="product">
+                <div className="product-image">
+                <p>Free Shipping</p>
+                <div className="product-image-front">
+                <img src="/static/products/100_1.jpg" />
+                </div>
+                <div className="product-image-back">
+                    <img src="/static/products/101_1.jpg" />
+                    </div>
+                </div>
+                <div className="product-title">
+                    <h2>Product One</h2>
+                    <div>
+                    <p>Dollars</p>
+                    <p>size</p>
+                    </div>
+                </div>
+                <button>Click here</button>
+            </div>
+            </div>
+        </div>
+        </>
+    )
+}
+
+export default App;
